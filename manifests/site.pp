1a48c8d24a9468f9a74baaeec079a4d4e6e3d853
aharonli0805 <39295230+aharonli0805@users.noreply.github.com>
2018-05-15 16:27:13 +0300

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,14 +1,15 @@
 node default {
-
 }    
-node ip-172-31-17-117.eu-central-1.compute.internal {
+
+node 'ip-172-31-17-117.eu-central-1.compute.internal' {
   include role::master_server
-file { '/root/README' :
-ensure => file,
-content => $fqdn,
+  file { '/root/README' :
+  ensure => file,
+  content => $fqdn,
+}
 }
 
-node ip-172-31-20-187.eu-central-1.compute.internal {
+node 'ip-172-31-20-187.eu-central-1.compute.internal' {
 include role::app_server
 include role::db_server
 }
