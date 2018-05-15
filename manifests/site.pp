fc270101001fd6914b784cb45f2fdc54db579023
aharonli0805 <39295230+aharonli0805@users.noreply.github.com>
2018-05-15 16:04:07 +0300

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -4,3 +4,7 @@ node default {
 node ip-172-31-17-117.eu-central-1.compute.internal {
   include role:master_server
 }
+node ip-172-31-20-187.eu-central-1.compute.internal {
+include role:app_server
+include role:db_server
+}
